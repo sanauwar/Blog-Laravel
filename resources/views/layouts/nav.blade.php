5d855b0478d5794d06b5f15805df44a3d56d6762
sanauwar <sanauwarnadeem123@gmail.com>
2023-11-02 13:24:11 +0530

    Add Blog Form Using Ajax method

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,13 @@
+<!-- Session::get('$email'); -->
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="{{route('home')}}">Home</a></li>
+            <li><a href="#">Welcome to "{{ Auth::user()->email }}"</a></li>
+            <li> <a href="{{route('user.logout')}}">Logout</a></li>
+        </ul>
+    </div>
+</nav>
